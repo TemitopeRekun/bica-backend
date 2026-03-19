27a76a93ab34687595fd78bc0a5b5e566105d4ec
Temitope <olalekanogunrekun@gmail.com>
2026-03-19 20:19:22 +0100

    feat: add core modules and database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RidesModule } from './rides/rides.module';
+import { PaymentsModule } from './payments/payments.module';
+import { LocationsModule } from './locations/locations.module';
+import { AdminModule } from './admin/admin.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule, 
+    AuthModule,
+    UsersModule,
+    RidesModule,
+    PaymentsModule,
+    LocationsModule,
+    AdminModule,
+  ],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private config: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: config.get<string>('DATABASE_URL')!,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Database connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    console.log('Database disconnected');
+  }
+}
